0340cadb989bc3c97121f6896b22a09eb0f5e321
Jonathan Sick <jsick@lsst.org>
2019-03-11 14:28:41 -0700

    Handle file_select action with cookiecutter vars
    This commit demonstrates how to use the variables from cookiecutter.json
    to populate the fields in a Slack dialog. This replaces static boilerplate.
    
    This exposes a few key limitations:
    
    1. The variable name and label fields can only be 75 characters long.
       This needs to be handled in templatekit (make alternative names for
       Slack?)
    
    2. There can only be up to 5 elements in a dialog. Again, this needs to
       be handled in templatekit. If a template has more questions, templatekit
       needs to provide a shim to help us craft an abbreviated form that either
       combines multiple options in selection dialogs, or explicitly ignores
       variables where the default is likely useful.

--- templatebot/slack/handlers/fileselect.py
+++ templatebot/slack/handlers/fileselect.py
@@ -3,11 +3,30 @@
 
 __all__ = ('handle_file_select_action',)
 
+import json
+from pathlib import Path
 import uuid
 
 
 async def handle_file_select_action(*, event_data, action_data, logger, app):
     """Handle the selection from a ``templatebot_file_select`` action ID.
+
+    The key steps are:
+
+    1. Use the ``chat.update`` method to replace the original message
+       containing a selection menu with a confirmation message. This prevents
+       someone from interacting with the menu again.
+    2. Open a Slack dialog to let the user fill in template variables based
+       on the ``cookiecutter.json`` file.
+    """
+    await _confirm_selection(event_data=event_data, action_data=action_data,
+                             logger=logger, app=app)
+    await _open_dialog(event_data=event_data, action_data=action_data,
+                       logger=logger, app=app)
+
+
+async def _confirm_selection(*, event_data, action_data, logger, app):
+    """Confirm the menu selection by replacing the menu with a static message.
     """
     httpsession = app['root']['api.lsst.codes/httpSession']
     headers = {
@@ -51,52 +70,29 @@ async def handle_file_select_action(*, event_data, action_data, logger, app):
             'Got a Slack error from chat.update',
             contents=response_json)
 
+
+async def _open_dialog(*, event_data, action_data, logger, app):
+    """Open a Slack dialog containing fields based on the user query.
+    """
+    selected_template = action_data['selected_option']['value']
+    elements = _create_dialog_elements(
+        template_name=selected_template,
+        app=app)
     dialog_body = {
         'trigger_id': event_data['trigger_id'],
         'dialog': {
-            "title": "Create a file",
+            "title": selected_template,
             "callback_id": f'templatebot_file_dialog_{str(uuid.uuid4())}',
             'state': '',
             'notify_on_cancel': True,
-            'elements': [
-                {
-                    "label": "Email Address",
-                    "name": "email",
-                    "type": "text",
-                    "subtype": "email",
-                    "placeholder": "you@example.com"
-                },
-                {
-                    "label": "Additional information",
-                    "name": "comment",
-                    "type": "textarea",
-                    "hint": "Provide additional information if needed."
-                },
-                {
-                    "label": "Meal preferences",
-                    "type": "select",
-                    "name": "meal_preferences",
-                    "options": [
-                        {
-                            "label": "Hindu (Indian) vegetarian",
-                            "value": "hindu"
-                        },
-                        {
-                            "label": "Strict vegan",
-                            "value": "vegan"
-                        },
-                        {
-                            "label": "Kosher",
-                            "value": "kosher"
-                        },
-                        {
-                            "label": "Just put it in a burrito",
-                            "value": "burrito"
-                        }
-                    ]
-                }
-            ]
-        },
+            'elements': elements
+        }
+    }
+
+    httpsession = app['root']['api.lsst.codes/httpSession']
+    headers = {
+        'content-type': 'application/json; charset=utf-8',
+        'authorization': f'Bearer {app["root"]["templatebot/slackToken"]}'
     }
     url = 'https://slack.com/api/dialog.open'
     async with httpsession.post(url, json=dialog_body, headers=headers) \
@@ -107,5 +103,76 @@ async def handle_file_select_action(*, event_data, action_data, logger, app):
             response=response_json)
     if not response_json['ok']:
         logger.error(
-            'Got a Slack error from chat.update',
+            'Got a Slack error from dialog.open',
             contents=response_json)
+
+
+def _create_dialog_elements(*, template_name, app):
+    repo = app['templatebot/repo'].get_repo(
+        gitref=app['root']['templatebot/repoRef']
+    )
+    template = repo[template_name]
+    cookiecutter_path = Path(template.cookiecutter_json_path)
+    cookiecutter_options = json.loads(cookiecutter_path.read_text())
+
+    elements = []
+    for var_name, option in cookiecutter_options.items():
+        if var_name.startswith('_'):
+            # Private variables (like _extensions) shouldn't be exposed in
+            # the dialog.
+            continue
+        elif isinstance(option, list):
+            element = _generate_select_element(var_name=var_name,
+                                               options=option)
+        else:
+            element = _generate_text_element(var_name=var_name,
+                                             default=option)
+        elements.append(element)
+
+    if len(elements) > 5:
+        # This is the maximum length supported by Slack dialogs at the moment.
+        elements = elements[:5]
+
+    return elements
+
+
+def _generate_select_element(*, var_name, options):
+    """Generate the JSON specification of a ``select`` element in a dialog.
+    """
+    short_var_name = var_name
+    if len(short_var_name) > 75:
+        # FIXME: max allowable length by Slack
+        short_var_name = short_var_name[:75]
+    option_elements = []
+    for v in options:
+        if len(v) > 75:
+            # FIXME: max allowable length by Slack
+            v = v[:75]  # max allowed length by Slack
+        option_elements.append({'label': v, 'value': v})
+    element = {
+        'label': short_var_name,
+        'type': 'select',
+        'name': short_var_name,
+        'options': option_elements
+    }
+    return element
+
+
+def _generate_text_element(*, var_name, default):
+    """Generate the JSON specification of a text element in a dialog.
+    """
+    short_var_name = var_name
+    if len(short_var_name) > 75:
+        # FIXME: max allowable length by Slack
+        short_var_name = short_var_name[:75]
+    short_default = default
+    if len(short_default) > 75:
+        # FIXME: max allowable length by Slack
+        short_default = short_default[:75]
+    element = {
+        'label': short_var_name,
+        'name': short_var_name,
+        "type": "text",
+        "placeholder": short_default
+    }
+    return element
