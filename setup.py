bd1827192b7a1843def90ca285193897c94ed401
Jonathan Sick <jsick@lsst.org>
2020-06-15 16:30:04 -0400

    Update to kafkit 0.2.0b3
    Mostly infrastructure improvements, as well as the new
    kafkit.registery.manager module.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = [
     'colorama==0.4.1',  # used by structlog
     'click>=6.7,<7.0',
     'fastavro==0.21.16',
-    'kafkit==0.1.1',
+    'kafkit==0.2.0b3',
     'aiokafka==0.5.2',
     'templatekit==0.3.0',
     'confluent-kafka==0.11.6',
