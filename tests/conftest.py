a3cd8a478bf6756404ac3ffdaef3127b1ece26a6
Jonathan Sick <jsick@lsst.org>
2024-09-16 12:41:05 -0400

    Update conftest.py for FastAPI apps

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,34 @@
-import pytest
+"""Test fixtures for unfurlbot tests."""
 
-from templatebot.app import create_app
+from __future__ import annotations
+
+from collections.abc import AsyncIterator
+
+import httpx
+import pytest_asyncio
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
+from httpx import AsyncClient
+
+from templatebot import main
 
 
-@pytest.fixture
-async def client(aiohttp_client):
-    app = create_app()
-    client = await aiohttp_client(app)
-    return client
+@pytest_asyncio.fixture
+async def app() -> AsyncIterator[FastAPI]:
+    """Return a configured test application.
+
+    Wraps the application in a lifespan manager so that startup and shutdown
+    events are sent during test execution.
+    """
+    async with LifespanManager(main.app):
+        yield main.app
+
+
+@pytest_asyncio.fixture
+async def client(app: FastAPI) -> AsyncIterator[AsyncClient]:
+    """Return an ``httpx.AsyncClient`` configured to talk to the test app."""
+    transport = httpx.ASGITransport(app=app)
+    async with AsyncClient(
+        transport=transport, base_url="https://example.com/"
+    ) as client:
+        yield client
--- tests/test_healthcheck.py
+++
@@ -1,5 +0,0 @@
-async def test_healthcheck():
-    # TODO we need a simulated Kafka in order to actually test as it stands now
-    # response = await client.get("/")
-    # assert response.status == 200
-    pass
