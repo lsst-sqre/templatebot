6b79a6e18696cf7fc13b9fd40352a2bfac868e0e
Jonathan Sick <jsick@lsst.org>
2024-09-18 17:15:20 -0400

    Improve SlackChatPostMessageRequest model
    - Add thread support
    - Add validation of the text fallback for blocks

--- src/templatebot/storage/slack/_models.py
+++ src/templatebot/storage/slack/_models.py
@@ -2,22 +2,51 @@
 
 from __future__ import annotations
 
-from pydantic import BaseModel, Field
+from typing import Self
+
+from pydantic import BaseModel, Field, model_validator
 
 from .blockkit import SlackBlock
 
 
 class SlackChatPostMessageRequest(BaseModel):
-    """A request body for the Slack ``chat.postMessage`` method."""
+    """A request body for the Slack ``chat.postMessage`` method.
+
+    See https://api.slack.com/methods/chat.postMessage for more information.
+    """
 
     channel: str = Field(
         ..., description="The channel ID.", examples=["C1234567890"]
+    )
+
+    thread_ts: str | None = Field(
+        None,
+        description="The timestamp of the parent message in a thread.",
+    )
+
+    reply_broadcast: bool | None = Field(
+        None,
+        description=(
+            "Whether to broadcast the message to the channel from a thread "
+            "(see ``thread_ts``)."
+        ),
     )
 
     text: str | None = Field(
         None, description="The text of the message as a fallback for blocks."
     )
 
+    mrkdwn: bool = Field(
+        True, description="Whether the text is formatted using Slack markdown."
+    )
+
     blocks: list[SlackBlock] | None = Field(
         None, description="The blocks that make up the message."
     )
+
+    @model_validator(mode="after")
+    def validate_text_fallback(self) -> Self:
+        """Ensure that the text field is provided if blocks are not."""
+        if not self.text and not self.blocks:
+            raise ValueError("Either `text` or `blocks` must be provided.")
+        return self
