721818ac63b05ed53a9a6ae7799ea4db49679c1a
Jonathan Sick <jsick@lsst.org>
2024-09-30 16:58:32 -0400

    Add SlackChatUpdateMessageRequest to slack package

--- src/templatebot/storage/slack/__init__.py
+++ src/templatebot/storage/slack/__init__.py
@@ -1,6 +1,10 @@
 """Slack API client and models."""
 
 from ._client import SlackWebApiClient
-from ._models import SlackChatPostMessageRequest
+from ._models import SlackChatPostMessageRequest, SlackChatUpdateMessageRequest
 
-__all__ = ["SlackWebApiClient", "SlackChatPostMessageRequest"]
+__all__ = [
+    "SlackWebApiClient",
+    "SlackChatPostMessageRequest",
+    "SlackChatUpdateMessageRequest",
+]
